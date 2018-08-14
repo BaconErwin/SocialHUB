6f27ef55cbf9f3f68b77557c0150110fd998fd3d
Admin-PC\Admin <erwin.gallo@yahoo.com>
2018-08-14 14:12:10 +0800

    Setting up

---
+++ ErwinGallo.SocialHUB/ErwinGallo.SocialHUB.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ErwinGallo.SocialHUB.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
